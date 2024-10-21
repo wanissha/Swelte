4d9c41618d22d3b8a0d011892b4e309a52e8bfb9
Wanissha <152176039+wanissha@users.noreply.github.com>
2024-10-21 22:16:41 +0700

    add dist in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+// import { defineConfig } from 'vite'
+// import { svelte } from '@sveltejs/vite-plugin-svelte'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [svelte()],
+// })
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+
 export default defineConfig({
   plugins: [svelte()],
-})
+  build: {
+    outDir: 'dist'  // Ensure this matches your firebase.json
+  }
+});
